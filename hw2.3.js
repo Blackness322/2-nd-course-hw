a523ca2f2fefebc76d62d0bb006ee8c6496cb434
Blackness322~ <d.yyldyz@gmail.com>
2023-02-24 16:46:17 +0300

    added hw2.3.js

---
+++ hw2.3.js
@@ -0,0 +1,79 @@
+
+// Задание 1
+let password = 'сложныйПароль';
+let answer = prompt('Введите пароль');
+if (answer === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно')
+}
+//задание 2
+let c = 7;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+//задание 3
+let d = 5041;
+let e = 30;
+if (d || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+//задание 4
+let a = 2;
+let b = 3;
+alert(a + b);
+
+//задание 5
+let monthNumber = Number(prompt('Введите номер месяца'));
+switch (monthNumber) {
+    case 1:
+        console.log('Январь');
+        break;
+    case 2:
+        console.log('Февраль');
+        break;
+    case 3:
+        console.log('Март');
+        break;
+    case 4:
+        console.log('Апрель');
+        break;
+    case 5:
+        console.log('Май');
+        break;
+    case 6:
+        console.log('Июнь');
+        break;
+    case 7:
+        console.log('Июль');
+        break;
+    case 8:
+        console.log('Август');
+        break;
+    case 9:
+        console.log('Сентябрь');
+        break;
+    case 10:
+        console.log('Октябрь');
+        break;
+    case 11:
+        console.log('Ноябрь');
+        break;
+    case 12:
+        console.log('Декабрь');
+        break;
+    default: 
+    console.log('номер месяца больше 13');
+        break;
+}
+//задание 6
+
+//задание 7
+
+//задание 8
+
+//задание 9
