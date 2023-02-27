f09d86160438dd04c140809c61a7190bd63e1f5a
Blackness322~ <d.yyldyz@gmail.com>
2023-02-27 17:45:18 +0300

    new file:   hw2.4.js renamed:    hw2.2.html -> hwtest.html

---
+++ hw2.4.js
@@ -0,0 +1,46 @@
+
+//Задание 1
+let i = 1;
+
+while (i <= 2) {
+    console.log('Привет');
+    i++;
+}
+
+//Задание 2
+
+let f = 1;
+
+while (f < 6) {
+    console.log(f);
+    f++;
+}
+
+//задание 3 
+
+let c = 7;
+
+while (c <= 22) {
+    console.log(c);
+    c++;
+}
+
+//Задание 4
+
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
+};
+for (let salary in obj) {
+    console.log(`${salary} — зарплата ${obj[salary]} долларов`);
+}
+
+//Задание 5
+
+for (let n = 1000, num = 4; n > 50; n /= 2) {
+        console.log(n);
+    }
+
+//Задание 6
+//Я не понял как это реализовать.
