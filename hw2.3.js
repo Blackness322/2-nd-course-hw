d4cf55c960c0f58f540dfc5bdfee679e7ad88daf
Blackness322~ <d.yyldyz@gmail.com>
2023-02-27 14:30:31 +0300

    fixed vars in js file

--- hw2.3.js
+++ hw2.3.js
@@ -15,56 +15,56 @@ if (c > 0 && c < 10) {
     console.log('Неверно');
 }
 //задание 3
-let d = 5041;
-let e = 30;
+const d = 5041;
+const e = 30;
 if (d || e > 100) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 //задание 4
-let a = 2;
-let b = 3;
+const a = 2;
+const b = 3;
 alert(a + b);
 
 //задание 5
-let monthNumber = Number(prompt('Введите номер месяца'));
+const monthNumber = Number(prompt('Введите номер месяца'));
 switch (monthNumber) {
     case 1:
-        console.log('Январь');
+        console.log('Зима');
         break;
     case 2:
-        console.log('Февраль');
+        console.log('Зима');
         break;
     case 3:
-        console.log('Март');
+        console.log('Весна');
         break;
     case 4:
-        console.log('Апрель');
+        console.log('Весна');
         break;
     case 5:
-        console.log('Май');
+        console.log('Весна');
         break;
     case 6:
-        console.log('Июнь');
+        console.log('Лето');
         break;
     case 7:
-        console.log('Июль');
+        console.log('Лето');
         break;
     case 8:
-        console.log('Август');
+        console.log('Лето');
         break;
     case 9:
-        console.log('Сентябрь');
+        console.log('Осень');
         break;
     case 10:
-        console.log('Октябрь');
+        console.log('Осень');
         break;
     case 11:
-        console.log('Ноябрь');
+        console.log('Осень');
         break;
     case 12:
-        console.log('Декабрь');
+        console.log('Зима');
         break;
     default: 
     console.log('номер месяца больше 13');
